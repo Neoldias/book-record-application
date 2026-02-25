01b36ddb4f02919f868c996f0f7890adb5a5324c
neoldias <neoldias1403@gmail.com>
2026-02-25 11:49:11 +0530

    done

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
+const app = express();
+
+
+const PORT = 8081;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is up and running succesfully",
+  });
+
+
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    message: "This route doesn't exist",
+  });
+});  
+
+});app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
